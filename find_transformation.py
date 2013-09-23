5a51de110992b4e6bf925b34486b9c582c80263d
Dan Perron <github@djperron.com>
2013-09-22 23:33:43 -0700

    first attempt at multithreading

--- find_transformation.py
+++ find_transformation.py
@@ -1,6 +1,11 @@
+#!/usr/bin/python
+
 from collections import deque
 import string
 import time
+import thread
+from multiprocessing import Process, Queue
+import math
 
 
 valid_letters = list(string.ascii_lowercase)
@@ -24,31 +29,60 @@ def import_file():
     words.add(word)
   return words
 
-def copy_and_append(current_list, new_item):
-  new_list = list(current_list)
-  new_list.append(new_item)
-  return new_list
+def search_word_set(word_objs, finish, dictionary, already_visited_words,
+                    ret_val):
+  queued_words = set()
+  queued_word_objs = list()
+  for word_obj in word_objs:
+    transformations = find_transformations(word_obj[0])
+    for transformation in transformations:
+      if transformation == finish:
+        path = list(word_obj[1])
+        path.append(word_obj[0])
+        path.append(transformation)
+        ret_val.put((True, path))
+        return
+      if (transformation in dictionary and 
+          transformation not in already_visited_words and
+          transformation not in queued_words):
+        queued_words.add(transformation)
+        path = list(word_obj[1])
+        path.append(word_obj[0])
+        queued_word_objs.append((transformation, path))
+  ret_val.put((False, queued_word_objs, queued_words))
 
 def find_path(start, finish, dictionary):
-  word_queue = deque([(start, list())])
+#  word_queue = deque([(start, list())])
+  queued_word_objs = list(([start, list()],))
   queued_words = set(start)
+  # make this a global var, or something
+  num_processes = 4.0
   while 1:
-    if len(word_queue) == 0:
+    queue_length = len(queued_word_objs)
+    if queue_length == 0:
       # return an empty list if we couldn't find a path
       return list()
-    word_obj = word_queue.popleft()
-    transformations = find_transformations(word_obj[0])
-    for word in transformations:
-      if word == finish:
-        path = list(word_obj[1])
-        path.append(word_obj[0])
-        path.append(word)
-        return path
-      if word in dictionary and word not in queued_words:
-        queued_words.add(word)
-        path = list(word_obj[1])
-        path.append(word_obj[0])
-        word_queue.append((word, path))
+    process_returns = Queue()
+    processes = list()
+    chunk_size = int(math.ceil(queue_length/num_processes))
+    for chunk_start in range(0, queue_length, chunk_size):
+      p = Process(target=search_word_set, 
+                  args=(queued_word_objs[chunk_start:chunk_start + chunk_size],
+                        finish, dictionary, queued_words, process_returns))
+      p.start()
+      processes.append(p)
+
+    # clear the work queue
+    queued_word_objs = list()
+
+    for process in range(0, queue_length, chunk_size):
+      ret_val = process_returns.get()
+      if ret_val[0]:
+        # done!
+        return ret_val[1]
+      else:
+        queued_word_objs.extend(ret_val[1])
+        queued_words.intersection_update(ret_val[2])
 
 def timed_find_path(start, finish, dictionary):
   start_time = time.time()
@@ -65,8 +99,8 @@ def main():
     word_two = raw_input('Please enter the second word: ')
     path = timed_find_path(word_one, word_two, valid_words)
     if len(path) == 0:
-      print ('could not find a series of transformations between ', word_one,
-             'and', word_two)
+      print ('could not find a series of transformations between word_one '
+             'and word_two')
     else:
       print 'found a series of transformations:'
       for word in path:
---
+++ find_transformation_test.py
@@ -0,0 +1,43 @@
+import find_transformation
+import inspect
+
+def test_same_word():
+  path = find_transformation.find_path('until', 'until',
+                                       find_transformation.import_file())
+  assert len(path) == 1
+
+def test_brain_bread():
+  path = find_transformation.find_path('brain', 'bread',
+                                       find_transformation.import_file())
+  assert len(path) == 3
+
+def test_smart_brain():
+  path = find_transformation.find_path('smart', 'brain',
+                                       find_transformation.import_file())
+  assert len(path) == 9
+
+def test_no_connection():
+  path = find_transformation.find_path('smart', 'until',
+                                       find_transformation.import_file())
+  assert len(path) == 0
+
+def test_wrong_length():
+  # todo: decide if it's an error case that
+  # 1) the provided words don't exist in the dictonary
+  # 2) there are no 3 letter words in the dictionary
+  path = find_transformation.find_path('abc', 'def',
+                                       find_transformation.import_file())
+  assert len(path) == 0
+
+def main():
+  # run tests
+  # todo: inspect this and just run all methods that start with test_
+  test_same_word()
+  test_brain_bread()
+  test_smart_brain()
+  test_no_connection()
+  test_wrong_length()
+  
+
+if __name__ == '__main__':
+  main()
